a99bb033f46b3a07d64d4a18c804a2dec0d0c103
Ali Osman Zengin <aliosmanzengin0212@gmail.com>
2024-07-24 15:03:13 +0200

    craete 03 folder in spring core

---
+++ 02-spring-boog-spring-core/03-setter-injection/src/main/java/com/azengin/springcoredemo/SpringcoredemoApplication.java
@@ -0,0 +1,17 @@
+package com.azengin.springcoredemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication(
+//		scanBasePackages = {"com.azengin.springcoredemo",
+//							"com.azengin.util"}
+//)
+@SpringBootApplication
+public class SpringcoredemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcoredemoApplication.class, args);
+	}
+
+}
